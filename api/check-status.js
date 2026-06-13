1cf7c5d1e989eab59d13d956be40c3705797fa4f
kreaverseai-dev <kreaverse.ai@gmail.com>
2026-06-14 00:16:48 +0700

    Update api/check-status.js

--- api/check-status.js
+++ api/check-status.js
@@ -2,8 +2,8 @@
 
 export default async function handler(req, res) {
     // Izinkan koneksi langsung dari Dasbor Kreaverse
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
